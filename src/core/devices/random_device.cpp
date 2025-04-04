846804421615cfe08db92e2482f85025b252bf9c
Amila Rathnayake <amilamad>
2025-04-04 14:29:13 -0400

    ctime header added

--- src/core/devices/random_device.cpp
+++ src/core/devices/random_device.cpp
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: Copyright 2024 shadPS4 Emulator Project
 // SPDX-License-Identifier: GPL-2.0-or-later
 
+#include <ctime>
 #include <cstdlib>
 #include "common/logging/log.h"
 #include "random_device.h"
